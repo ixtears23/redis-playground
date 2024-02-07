6d7000797b6b7e65571834f0b3ab85169f1972e2
JunseokOh <ixtears23@icloud.com>
2024-02-08 00:35:09 +0900

    프로젝트 생성

---
+++ src/test/java/junseok/snr/redisplayground/RedisPlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package junseok.snr.redisplayground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisPlaygroundApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
