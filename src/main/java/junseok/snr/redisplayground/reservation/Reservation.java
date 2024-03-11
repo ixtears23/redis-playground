40fff152aa0a9f86257a3684feb62b40c0d64003
junseokoh0 <junseok@bodycodi.com>
2024-03-11 19:12:58 +0900

    Reservation 객체 RedisHash 로 수정

--- src/main/java/junseok/snr/redisplayground/reservation/Reservation.java
+++ src/main/java/junseok/snr/redisplayground/reservation/Reservation.java
@@ -4,13 +4,19 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
+import org.springframework.data.redis.core.index.Indexed;
 
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
+@RedisHash(value = "reservations", timeToLive = 300L)
 public class Reservation {
+    @Id
     private String id;
+    @Indexed
     private String name;
     private String roomNumber;
 }
