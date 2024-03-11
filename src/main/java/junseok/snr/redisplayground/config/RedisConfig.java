4964d450e2222a019f2f1db600af3e42055e652c
JunseokOh <ixtears23@icloud.com>
2024-03-12 00:04:29 +0900

    RedisTemplate HashSerializer 설정 추가

--- src/main/java/junseok/snr/redisplayground/config/RedisConfig.java
+++ src/main/java/junseok/snr/redisplayground/config/RedisConfig.java
@@ -12,6 +12,7 @@ import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
@@ -43,6 +44,9 @@ public class RedisConfig {
         redisTemplate.setConnectionFactory(lettuceConnectionFactory());
         redisTemplate.setKeySerializer(new StringRedisSerializer());
         redisTemplate.setValueSerializer(new StringRedisSerializer());
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashValueSerializer(new Jackson2JsonRedisSerializer<>(Object.class));
+
         return redisTemplate;
     }
     @Bean
