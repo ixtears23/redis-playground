3b7da63785072f30710257a4073bb5234e1446d4
JunseokOh <ixtears23@icloud.com>
2024-04-02 01:04:39 +0900

    다음 작업에 대한 TODO 코멘트 작성

--- src/main/java/junseok/snr/redisplayground/order/infrastructure/OrderMessageReceiver.java
+++ src/main/java/junseok/snr/redisplayground/order/infrastructure/OrderMessageReceiver.java
@@ -13,4 +13,7 @@ public class OrderMessageReceiver {
     public void receiveMessage(String message, @Header(value = "SendId", required = false) String senderId) {
         log.info("=== Received Message : {} from senderId : {}", message, senderId);
     }
+
+
+    // TODO @SqsListener 가 아닌, 직접 수신 받고, 로직 처리 후, 메시지 삭제, DLQ 전송되도록 강제 실패 및 DLQ 직접 전송
 }
