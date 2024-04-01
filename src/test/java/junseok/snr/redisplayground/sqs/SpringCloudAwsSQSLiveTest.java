131d9f2f2fcbaadcf435195e2033a0fdd1ef3b5b
JunseokOh <ixtears23@icloud.com>
2024-04-01 22:39:51 +0900

    RecordQueue 테스트 코드 추가

--- src/test/java/junseok/snr/redisplayground/sqs/SpringCloudAwsSQSLiveTest.java
+++ src/test/java/junseok/snr/redisplayground/sqs/SpringCloudAwsSQSLiveTest.java
@@ -2,11 +2,13 @@ package junseok.snr.redisplayground.sqs;
 
 import io.awspring.cloud.sqs.operations.SqsTemplate;
 import junseok.snr.redisplayground.common.properties.EventQueuesProperties;
+import junseok.snr.redisplayground.user.application.UserCreatedEvent;
 import junseok.snr.redisplayground.user.infrastructure.UserRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.time.Duration;
+import java.util.UUID;
 
 import static org.awaitility.Awaitility.await;
 
@@ -29,6 +31,18 @@ class SpringCloudAwsSQSLiveTest extends BaseSqsIntegrationTest {
         await().atMost(Duration.ofSeconds(3))
                 .until(() -> userRepository.findByName(userName)
                                 .isPresent());
+    }
+
+    @Test
+    void givenARecordPayload_whenSend_shouldReceive() {
+        String userId = UUID.randomUUID().toString();
+
+        var payload = new UserCreatedEvent(userId, "JunseokOh", "ixtears@naver.com");
+
+        sqsTemplate.send(to -> to.queue(eventQueuesProperties.getUserCreatedRecordQueue()).payload(payload));
+
+        await().atMost(Duration.ofSeconds(3))
+                .until(() -> userRepository.findById(userId).isPresent());
     }
 
 
