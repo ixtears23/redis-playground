bb341b08a092348d475d125a5157b429d2b17acb
JunseokOh <ixtears23@icloud.com>
2024-04-06 00:45:43 +0900

    AWS 계정 설정 변경 - 프로파일 적용

--- src/main/java/junseok/snr/redisplayground/common/config/SqsConfig.java
+++ src/main/java/junseok/snr/redisplayground/common/config/SqsConfig.java
@@ -2,16 +2,20 @@ package junseok.snr.redisplayground.common.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.sqs.SqsAsyncClient;
 import software.amazon.awssdk.services.sqs.SqsClient;
 
 @Configuration
 public class SqsConfig {
+    private static final ProfileCredentialsProvider credentialsProvider = ProfileCredentialsProvider.create("junseok");
+
 
     @Bean
     public SqsAsyncClient sqsAsyncClient() {
         return SqsAsyncClient.builder()
+                .credentialsProvider(credentialsProvider)
                 .region(Region.AP_NORTHEAST_2)
                 .build();
     }
@@ -19,6 +23,7 @@ public class SqsConfig {
     @Bean
     public SqsClient sqsClient() {
         return SqsClient.builder()
+                .credentialsProvider(credentialsProvider)
                 .region(Region.AP_NORTHEAST_2)
                 .build();
     }
